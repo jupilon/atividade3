c219cb85c9b89c93047d4aee756b1e3ed5e0d74d
jupilon <julianapilon2016@gmail.com>
2021-08-19 17:41:38 +0000

    calculo do gasto total e media

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -3,6 +3,8 @@ public class BalancoTrimestral {
         int gastosJaneiro=1500;
         int gastosFevereiro=2300;
         int gastosMarco=1700;
+        int gastoTotal = gastosJaneiro + gastosFevereiro + gastosMarco
+        int mediaGastos = gastoTotal/3
     }
 
 }
