28f4e241e18a142d83243fd08a40c20e635182e4
jupilon <julianapilon2016@gmail.com>
2021-08-19 17:38:20 +0000

    adicionado os gastos dos meses

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,3 +1,8 @@
 public class BalancoTrimestral {
-    
+    public static void main(String args[]){
+        int gastosJaneiro=1500;
+        int gastosFevereiro=2300;
+        int gastosMarco=1700;
+    }
+
 }
